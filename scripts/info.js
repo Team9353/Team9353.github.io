b0034ed1ad74629ba0d538ceb25c3faa08a0fe1e
skywheeler <li.hannah@gmail.com>
2019-11-17 14:39:59 -0500

    text transitions

--- scripts/info.js
+++ scripts/info.js
@@ -7,19 +7,35 @@ var node6 = document.querySelector('#test6');
 var node7 = document.querySelector('#test7');
 var node8 = document.querySelector('#test8');
 
+var text1 = document.querySelector("#text1")
+var text2 = document.querySelector("#text2")
+var text3 = document.querySelector("#text3")
+var text4 = document.querySelector("#text4")
+var text5 = document.querySelector("#text5")
+var text6 = document.querySelector("#text6")
+var text7 = document.querySelector("#text7")
+
 var nodeArray = [
     document.querySelector('#test3'),
     document.querySelector('#test4'),
     document.querySelector('#test5')
 ];
 
-ScrollReveal().reveal(node1, { delay: 5000 });
-ScrollReveal().reveal(node1, { duration: 5000 });
-ScrollReveal().reveal(node2, { duration: 5000 });
-ScrollReveal().reveal(node3, { duration: 5000 });
-ScrollReveal().reveal(node4, { duration: 5000 });
-ScrollReveal().reveal(node5, { duration: 5000 });
-ScrollReveal().reveal(node6, { duration: 5000 });
-ScrollReveal().reveal(node7, { duration: 5000 });
-ScrollReveal().reveal(node8, { duration: 5000 });
+
+ScrollReveal().reveal(node1, { duration: 2000 });
+ScrollReveal().reveal(text1, { delay: 100, duration: 3000})
+ScrollReveal().reveal(node2, { duration: 2000 });
+ScrollReveal().reveal(text2, { delay: 100, duration: 3000})
+ScrollReveal().reveal(node3, { duration: 2000 });
+ScrollReveal().reveal(text3, { delay: 100, duration: 3000})
+ScrollReveal().reveal(node4, { duration: 2000 });
+ScrollReveal().reveal(text4, { delay: 100, duration: 3000})
+ScrollReveal().reveal(node5, { duration: 2000 });
+ScrollReveal().reveal(text5, { delay: 100, duration: 3000})
+ScrollReveal().reveal(node6, { duration: 2000 });
+ScrollReveal().reveal(text6, { delay: 100, duration: 3000})
+ScrollReveal().reveal(node7, { duration: 2000 });
+ScrollReveal().reveal(text7, { delay: 100, duration: 3000})
+ScrollReveal().reveal(node8, { duration: 2000 });
+
 
